8b77f34d3e81d5d7cdfddaa0c73723cbf3a88ae9
Olexandr Panov <futupas@gmail.com>
2020-05-02 16:46:05 +0300

    disable web page preview when bot gives u order in channel

--- send_message.php
+++ send_message.php
@@ -2,7 +2,7 @@
 
 
 function SendMessage($chatid, $text) {
-    $response = file_get_contents('https://api.telegram.org/bot'.getenv('bot_token').'/sendMessage?chat_id='.$chatid.'&text='.urlencode($text));
+    $response = file_get_contents('https://api.telegram.org/bot'.getenv('bot_token').'/sendMessage?chat_id='.$chatid.'&text='.urlencode($text).'&disable_web_page_preview=true');
 };
 function ReplyToMessage($chatid, $text, $msgtoreply) {
     $response = file_get_contents(
