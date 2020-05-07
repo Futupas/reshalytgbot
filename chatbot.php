a2fe7851a692138a1fa8898ea399e9e69f363ade
Olexandr Panov <futupas@gmail.com>
2020-05-07 22:30:25 +0300

    fix bugs

--- chatbot.php
+++ chatbot.php
@@ -84,10 +84,10 @@
                         SendMessageToChatBot($msg_chatid, 'u replied on a wrong message');
                         exit(0);
                     }
-                    $user = get_user($msg_chatid);
-                    $text = "*".$user->name."*:\n$msg";
+                    // $user = get_user($msg_chatid);
+                    $text = "*".$user['name']."*:\n$msg";
                     $response = SendMessageWithMarkdownToChatBot($chat_message['destination_chat_id'], $text);
-                    add_row_to_chat_messages_table($chat_message['destination_chat_id'], $response->result->message_id, $msg_chatid, $chat_message['']);
+                    add_row_to_chat_messages_table($chat_message['destination_chat_id'], $response->result->message_id, $msg_chatid, $chat_message['order']);
                 } else {
                     SendMessageToChatBot($msg_chatid, 'u can not send me msg that is not a reply');
                     exit(0);
