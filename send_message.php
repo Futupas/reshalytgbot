90a4e6d415e7a8c4f0725ab5a24b7b3d60c9602a
Olexandr Panov <futupas@gmail.com>
2020-05-24 15:21:57 +0300

    fix bugs

--- send_message.php
+++ send_message.php
@@ -39,16 +39,16 @@ function SendMessageToChatBot($chatid, $text, $order) {
 function SendMessageWithMarkdownToChatBot($chatid, $text, $order) {
     $response;
     if ($chatid === 'c') {
-        $response = json_decode(file_get_contents('https://api.telegram.org/bot'.getenv('chat_bot_token').'/sendMessage?chat_id='.$chatid.'&text='.urlencode($text).'&disable_web_page_preview=true&parse_mode=markdown'));
+        $response = json_decode(file_get_contents('https://api.telegram.org/bot'.getenv('chat_bot_token').'/sendMessage?chat_id='.$order['customer_id'].'&text='.urlencode($text).'&disable_web_page_preview=true&parse_mode=markdown'));
         add_row_to_chat_messages_table_with_text($order['customer_id'], $response->result->message_id, $order['executor_id'], $order['id'], $text);
     } else if ($chatid === 'e') {
-        $response = json_decode(file_get_contents('https://api.telegram.org/bot'.getenv('chat_bot_token').'/sendMessage?chat_id='.$chatid.'&text='.urlencode($text).'&disable_web_page_preview=true&parse_mode=markdown'));
+        $response = json_decode(file_get_contents('https://api.telegram.org/bot'.getenv('chat_bot_token').'/sendMessage?chat_id='.$order['executor_id'].'&text='.urlencode($text).'&disable_web_page_preview=true&parse_mode=markdown'));
         add_row_to_chat_messages_table_with_text($order['executor_id'], $response->result->message_id, $order['customer_id'], $order['id'], $text);
     } else if ($chatid == $order['customer_id']) {
-        $response = json_decode(file_get_contents('https://api.telegram.org/bot'.getenv('chat_bot_token').'/sendMessage?chat_id='.$chatid.'&text='.urlencode($text).'&disable_web_page_preview=true&parse_mode=markdown'));
+        $response = json_decode(file_get_contents('https://api.telegram.org/bot'.getenv('chat_bot_token').'/sendMessage?chat_id='.$order['customer_id'].'&text='.urlencode($text).'&disable_web_page_preview=true&parse_mode=markdown'));
         add_row_to_chat_messages_table_with_text($order['customer_id'], $response->result->message_id, $order['executor_id'], $order['id'], $text);
     } else if ($chatid == $order['executor_id']) {
-        $response = json_decode(file_get_contents('https://api.telegram.org/bot'.getenv('chat_bot_token').'/sendMessage?chat_id='.$chatid.'&text='.urlencode($text).'&disable_web_page_preview=true&parse_mode=markdown'));
+        $response = json_decode(file_get_contents('https://api.telegram.org/bot'.getenv('chat_bot_token').'/sendMessage?chat_id='.$order['executor_id'].'&text='.urlencode($text).'&disable_web_page_preview=true&parse_mode=markdown'));
         add_row_to_chat_messages_table_with_text($order['executor_id'], $response->result->message_id, $order['customer_id'], $order['id'], $text);
     } else {
         $response = json_decode(file_get_contents('https://api.telegram.org/bot'.getenv('chat_bot_token').'/sendMessage?chat_id='.$chatid.'&text='.urlencode($text).'&disable_web_page_preview=true&parse_mode=markdown'));
